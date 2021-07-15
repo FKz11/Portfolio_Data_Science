95568aea3bf7343f1f487983d92ae06bf5497e90
FKz11 <rjcnz735@yandex.ru>
2021-07-15 14:39:47 +0300

    Matrix_Class

--- Matrix_Class/Matrix_Class.py
+++ Matrix_Class/Matrix_Class.py
@@ -13,6 +13,21 @@ def enter_n_lines_m_columns(line):
     return enter_n_lines_m_columns(line)
 
 
+def enter_vector(line, length):
+    try:
+        data: list = input(line).split(" ")
+        assert len(data) == length
+        for i in range(length):
+            if "." in list(data[i]):
+                data[i] = float(data[i])
+            else:
+                data[i] = int(data[i])
+        return data
+    except (AssertionError, ValueError):
+        print("Invalid input!!!")
+        return enter_vector(line, length)
+
+
 def enter_array(line, n_lines, m_columns):
     print(line)
     array = []
@@ -20,7 +35,7 @@ def enter_array(line, n_lines, m_columns):
         for _ in range(n_lines):
             data: list = input().split(" ")
             assert len(data) == m_columns
-            for i in range(len(data)):
+            for i in range(m_columns):
                 if "." in list(data[i]):
                     data[i] = float(data[i])
                 else:
@@ -72,6 +87,24 @@ class Matrix:
         else:
             return round(cls.det_array(matrix.array), cls.class_round)
 
+    @classmethod
+    def solution(cls, matrix, vector):
+        a_det = cls.det(matrix)
+        if a_det is None:
+            print("ERROR: a_det is None")
+            return None
+        elif a_det == 0:
+            print("ERROR: a_det == 0")
+            return None
+        else:
+            array = matrix.array
+            length = len(vector)
+            solve = dict()
+            for k in range(length):
+                solve["x" + str(k + 1)] = cls.det_array([array[i][:k] + [vector[i]] +
+                                                         array[i][k + 1:] for i in range(length)]) / a_det
+            return solve
+
     def __str__(self):
         data = ""
         for line in self.array:
@@ -82,6 +115,7 @@ class Matrix:
 
 
 if __name__ == "__main__":
+    print("Enter the matrices A and B to multiply them and find their determinants")
     A_n_lines = enter_n_lines_m_columns("Enter the number of lines of the matrix A\n>>>:")
     A_m_columns = enter_n_lines_m_columns("Enter the number of columns of matrix A\n>>>:")
     A_array = enter_array("Enter the values in the line of the matrix A separated by a space,"
@@ -90,10 +124,19 @@ if __name__ == "__main__":
     B_m_columns = enter_n_lines_m_columns("Enter the number of columns of matrix B\n>>>:")
     B_array = enter_array("Enter the values in the line of the matrix B separated by a space,"
                           " to move to the next line, press enter\n>>>:", B_n_lines, B_m_columns)
+    print("Enter a system of linear equations in the form of a matrix C and a vector of values V to find solutions")
+    C_n_lines = enter_n_lines_m_columns("Enter the number of lines of the matrix C\n>>>:")
+    C_m_columns = enter_n_lines_m_columns("Enter the number of columns of matrix C\n>>>:")
+    C_array = enter_array("Enter the values in the line of the matrix C separated by a space,"
+                          " to move to the next line, press enter\n>>>:", C_n_lines, C_m_columns)
+    V_vector = enter_vector("Enter the vector of values V separated by a space\n>>>:\n", C_n_lines)
     A_matrix = Matrix(A_array, A_n_lines, A_m_columns)
     B_matrix = Matrix(B_array, B_n_lines, B_m_columns)
+    C_matrix = Matrix(C_array, C_n_lines, C_m_columns)
     print("------------------------------------------")
     print(f"A*B:\n{Matrix.dot(A_matrix, B_matrix)}\n")
     print(f"B*A:\n{Matrix.dot(B_matrix, A_matrix)}\n")
     print(f"Determinant A:\n{Matrix.det(A_matrix)}\n")
     print(f"Determinant B:\n{Matrix.det(B_matrix)}\n")
+    print(f'Solution of a system of linear equations given by a matrix C '
+          f'and a vector of values V:\n{Matrix.solution(C_matrix, V_vector)}\n')
